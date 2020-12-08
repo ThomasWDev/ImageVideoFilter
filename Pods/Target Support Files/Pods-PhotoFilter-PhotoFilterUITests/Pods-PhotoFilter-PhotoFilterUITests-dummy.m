f568d4ceaecb5bc3a869d7c80a091f5a153ece57
Thomas Woodfin <tommy.woodfin@gmail.com>
2020-12-09 02:19:03 +0600

    remove pods

--- Pods/Target Support Files/Pods-PhotoFilter-PhotoFilterUITests/Pods-PhotoFilter-PhotoFilterUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoFilter_PhotoFilterUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoFilter_PhotoFilterUITests
-@end
--- Pods/Target Support Files/Pods-PhotoFilter/Pods-PhotoFilter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoFilter : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoFilter
-@end
